bc474fd2fa21e78ece84942e546c99d4b4e3760f
Alessandro Saggese <asaggese@gmail.com>
2022-12-29 19:45:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const totalSlot = document.querySelector('.total-slot');
+const giftsListElement = document.querySelector('.gifts-list');
+
+const form = document.querySelector('#gift-form');
+const nameField = document.querySelector('#name-field');
+const priceField = document.querySelector('#price-field');
+const giftField = document.querySelector('#gift-field');
